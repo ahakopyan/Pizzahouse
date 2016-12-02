2dd45c2e0dde24e89e9479ba49eceea6125785d3
Араик Акопян <araikaa@MacBook-Pro-Araik.local>
2016-12-02 12:36:22 +0300

    Add Rakefile, update Gemfile, add Product model

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+# подключение базы данных
+set : database, "sqlite3:pizzahouse.db"
+# создаем модель: то есть миграцию
+class Product < ActiveRecord::Base
+end
 
 get '/' do
 	erb :index
