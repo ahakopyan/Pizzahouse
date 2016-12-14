b589cb5bb5b4bd7eb854a0fc9ea9516da0fc6247
Араик Акопян <araikaa@MacBook-Pro-Araik.local>
2016-12-14 17:06:58 +0300

    Add cart_get_nuber_of_items function

--- db/migrate/20161202214208_add_products.rb
+++ db/migrate/20161202214208_add_products.rb
@@ -8,7 +8,7 @@ class AddProducts < ActiveRecord::Migration[5.0]
   		:is_spicy => false, 
   		:is_veg => false, 
   		:is_best_offer => false, 
-  		:path_to_image => '/images/hawaiian.jpeg'
+  		:path_to_image => '/images/hawaiian.png'
   		})
 
   	Product.create ({
@@ -18,7 +18,7 @@ class AddProducts < ActiveRecord::Migration[5.0]
   		:is_spicy => false, 
   		:is_veg => false, 
   		:is_best_offer => true, 
-  		:path_to_image => '/images/pepperoni.jpg'
+  		:path_to_image => '/images/pepperoni.png'
   		})
 
   	Product.create ({
@@ -29,7 +29,7 @@ class AddProducts < ActiveRecord::Migration[5.0]
   		:is_spicy => false, 
   		:is_veg => false, 
   		:is_best_offer => false, 
-  		:path_to_image => '/images/veg.jpg'
+  		:path_to_image => '/images/veg.png'
   		})
   end
 end
