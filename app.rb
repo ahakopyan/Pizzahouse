f20a9ff64cca89118a041c5d6307f7bedbd328b4
Араик Акопян <araikaa@MacBook-Pro-Araik.local>
2016-12-16 22:15:52 +0300

    Add cart page

--- app.rb
+++ app.rb
@@ -18,9 +18,17 @@ get '/about' do
 end
 
 post '/cart' do
+	# переменная orders_input для получения из Layout
 	orders_input = params[:orders]
-	@orders = parse_orders_input orders_input
-	erb "Hello! #{@orders.inspect}"
+	# переменная которая используется во views 
+	@items = parse_orders_input orders_input
+
+	# для каждого элемента делаем запрос
+	@items.each do |item|
+		# item - это у нас массив [id, cnt]
+		 item[0] = Product.find(item[0])
+	end
+	erb :cart
 end
 # функция, которая я разделяет строку продуктов в карзине на  три чассти в массивы:
 def parse_orders_input orders_input
